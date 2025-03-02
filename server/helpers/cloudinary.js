bf884d87ed02bf1cd2de10320ae68cbe22173c92
MinhDuy0i0909090 <nldm0908@gmail.com>
2025-03-02 14:17:18 +0700

    update env v3

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -1,10 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: "dy6mtc2ap",
-  api_key: "758761626261267",
-  api_secret: "qod4e9VECja96s6xWwFHE0ZaVFE",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 const storage = new multer.memoryStorage();
 
--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,11 +1,9 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: "sandbox",
-  client_id:
-    "AScodeBnuDTWZUOoltDF3pndelXITEdbENLtgrgtT1d8IrOb6LHJFGIHOUZ7ZMlFTn9xZw74Gs8pvS4C",
-  client_secret:
-    "EDMNFupo19zZFzOBFRRtYRao54pdUw3-Zhj5VoPpmE-VQb-FnswzD4NoZt7HeuhC1xHAa9ItaxU5oaVX",
+  mode: process.env.PAYPAL_MODE || "sandbox", 
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
 module.exports = paypal;
