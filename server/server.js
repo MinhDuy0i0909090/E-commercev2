9e146e4785d64f7f468ede722bf56c78fa2f327b
MinhDuy0i0909090 <nldm0908@gmail.com>
2025-03-02 15:22:38 +0700

    update server v2

--- server/server.js
+++ server/server.js
@@ -53,6 +53,9 @@ app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/review", shopReviewRouter);
 
 // app.listen(PORT, () => console.log(`PORT is running on ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.listen(process.env.PORT, () => {
   console.log(`Server is now running `);
 });
